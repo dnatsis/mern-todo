511d2bb7389ab00d614bf233ae709ff0a8fe9e06
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-02-24 14:41:18 -0500

    Created basic Todo frontend

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import CreateTodo from './components/CreateTodo/CreateTodo';
+import EditTodo from './components/EditTodo/EditTodo';
+import TodosList from './components/TodosList/TodosList';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">
+              MERN-Todo List
+            </Link>
+            <div className="collpase nav-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">
+                    Create Todo
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
