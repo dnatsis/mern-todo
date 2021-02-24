d567b357adb2e1e95a24fbab0c11a90a5badbdc8
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-02-24 17:03:32 -0500

    created the edit component

--- src/components/TodosList/TodosList.js
+++ src/components/TodosList/TodosList.js
@@ -4,9 +4,15 @@ import axios from 'axios';
 
 const Todo = (props) => (
   <tr>
-    <td>{props.todo.todo_description}</td>
-    <td>{props.todo.todo_responsible}</td>
-    <td>{props.todo.todo_priority}</td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_description}
+    </td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_responsible}
+    </td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_priority}
+    </td>
     <td>
       <Link to={'/edit/' + props.todo._id}>Edit</Link>
     </td>
@@ -20,6 +26,17 @@ class TodosList extends Component {
   }
 
   componentDidMount() {
+    axios
+      .get('http://localhost:4000/todos')
+      .then((response) => {
+        this.setState({ todos: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  componentDidUpdate() {
     axios
       .get('http://localhost:4000/todos')
       .then((response) => {
